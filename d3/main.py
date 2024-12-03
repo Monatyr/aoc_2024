f9d2f6684e1623a669fff89b8579de6cef15d729
Adam Przywieczerski <adam.przywieczerski@gmail.com>
2024-12-03 19:27:35 +0100

    Day 3 - regex

--- d3/main.py
+++ d3/main.py
@@ -1,4 +1,5 @@
 from utils.utils import *
+import re
 
 
 def get_result(line, part_two=False, enabled=True):
@@ -59,26 +60,53 @@ def get_result(line, part_two=False, enabled=True):
         n2 = int(line[i:j])
         if enabled:
             res += n1 * n2
-    return (res, None) if not part_two else (res, enabled)
+    return (res, enabled)
+
+
+def get_result_regex(line, part_two=False, enabled=True):
+    res = 0
+    if not part_two:
+        regex = r'mul\((?:[1-9][0-9]{0,2}|0),(?:[1-9][0-9]{0,2}|0)\)'
+    else:
+        regex = r'(?:mul\((?:[1-9][0-9]{0,2}|0),(?:[1-9][0-9]{0,2}|0)\))|do\(\)|don\'t\(\)'
+    matches = re.findall(regex, line)
+    for match in matches:
+        if part_two:
+            if match == "do()":
+                enabled = True
+                continue
+            elif match == "don't()":
+                enabled = False
+                continue
+        match = match[4:]
+        match = match[:len(match)-1]
+        n1, n2 = list(map(lambda x: int(x), match.split(',')))
+        if part_two and not enabled:
+            continue
+        res += n1 * n2
+    return res, enabled
 
 
 def p1(lines: list):
-    res = 0
+    res1, res2 = 0, 0
     for line in lines:
-        res += get_result(line)[0]
-    return res
-
+        res1 += get_result(line)[0]
+        res2 += get_result_regex(line)[0]
+    print(f"\n--- PART 1 ---\nNormal solution: {res1}\nRegex solution: {res2}")
+    return re
 
 
 def p2(lines: list):
-    res, enabled = 0, True
+    res1, res2, enabled1, enabled2 = 0, 0, True, True
     for line in lines:
-        part_res, enabled = get_result(line, True, enabled)
-        res += part_res
-    return res
+        part_res1, enabled1 = get_result(line, True, enabled1)
+        part_res2, enabled2 = get_result_regex(line, True, enabled2)
+        res1 += part_res1
+        res2 += part_res2
+    print(f"\n--- PART 2 ---\nNormal solution: {res1}\nRegex solution: {res2}")
 
 
 def solution():
     lines = get_lines('inputs/input3')
-    print(p1(lines))
-    print(p2(lines))
+    p1(lines)
+    p2(lines)
